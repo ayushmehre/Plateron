f2eda8acb08d046f2bad66e015a3e63b5110f1e1
Ayush Mehre <ayush.mehre@goniyo.com>
2023-12-07 16:44:33 +0530

    add: Menu Listing Screen

--- plateron/lib/main.dart
+++ plateron/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:plateron/providers/screen1_provider.dart';
-import 'package:plateron/screens/screen1.dart';
+import 'package:plateron/providers/cart_provider.dart';
+import 'package:plateron/providers/product_provider.dart';
+import 'package:plateron/repositories/product_repository.dart';
+import 'package:plateron/routes.dart';
+import 'package:plateron/screens/menu_listing_screen.dart';
+import 'package:plateron/screens/summary_screen.dart';
 import 'package:provider/provider.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -10,17 +15,48 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => CartProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ProductProvider(ProductRepository()),
+        ), // Move the ProductProvider here
+      ],
+      child: MaterialApp(
+        routes: {
+          Routes.menuListing: (context) => const MenuListingScreen(),
+          Routes.orderSummary: (context) => const OrderSummaryScreen(),
+        },
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          textTheme: const TextTheme(
+            headlineSmall: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            titleTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            iconTheme: IconThemeData(
+              color: Colors.black,
+            ),
+          ),
+          scaffoldBackgroundColor: Colors.white
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const MenuListingScreen(),
       ),
-      home: MultiProvider(providers: [
-        ChangeNotifierProvider(create: (_) => Screen1Provider()),
-      ], child: const Screen1())
     );
   }
 }
---
+++ plateron/lib/screens/menu_listing_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:plateron/providers/product_provider.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/cart_widget.dart';
+import '../widgets/product_item.dart';
+
+class MenuListingScreen extends StatefulWidget {
+  const MenuListingScreen({super.key});
+
+  @override
+  State<MenuListingScreen> createState() => _MenuListingScreenState();
+}
+
+class _MenuListingScreenState extends State<MenuListingScreen> {
+  @override
+  void initState() {
+    var productsProvider = Provider.of<ProductProvider>(context, listen: false);
+    productsProvider.fetchProducts();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Menu Listing'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Consumer<ProductProvider>(
+              builder: (context, provider, child) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: provider.productsByCategory.length,
+                  itemBuilder: (context, index) {
+                    final categoryName =
+                        provider.productsByCategory.keys.elementAt(index);
+                    final products = provider.productsByCategory[categoryName]!;
+
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Category header
+                        Container(
+                          width: double.infinity,
+                          color: Colors.grey.withOpacity(0.2),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 8.0),
+                          child: Text(
+                            categoryName,
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                        ),
+                        // List of product items for this category
+                        ListView.builder(
+                          itemCount: products.length,
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, innerIndex) {
+                            return ProductItemWidget(
+                              product: products[innerIndex],
+                            );
+                          },
+                        ),
+                        const SizedBox(height: 15),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+          const CartWidget(),
+        ],
+      ),
+    );
+  }
+}
